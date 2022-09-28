fd53fb4bcb26f2b9c91862ba9ab79258ed21359e
Nicolas DECRESSAC <101709161+Nilyss@users.noreply.github.com>
2022-09-28 20:28:57 +0200

    Chapter 3
     -Almost finish Directive part.
     -Starting exercice directive part.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { BorderCardDirective } from './border-card.directive'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BorderCardDirective],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
